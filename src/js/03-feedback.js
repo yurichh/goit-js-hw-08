5a16d82dbb4a20d018b0f4737e4fae800f3e57b7
unknown <velidchukaleksey@gmail.com>
2023-09-20 16:23:37 +0300

    final

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,38 @@
+import throttle from 'lodash.throttle';
+import lodash from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const emailInput = document.querySelector('input[name=email]');
+const messageInput = document.querySelector('textarea[name=message]');
+
+function saveInfo() {
+  const data = {
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+  localStorage.setItem('user-data', JSON.stringify(data));
+}
+
+function returnInfo() {
+  const savedInfo = localStorage.getItem('user-data');
+  if (savedInfo) {
+    const data = JSON.parse(savedInfo);
+    emailInput.value = data.email;
+    messageInput.value = data.message;
+  }
+}
+emailInput.addEventListener('input', throttle(saveInfo, 500));
+messageInput.addEventListener('input', throttle(saveInfo, 500));
+
+window.addEventListener('load', returnInfo);
+
+form.addEventListener('submit', function (event) {
+  event.preventDefault();
+  localStorage.removeItem('user-data');
+  const data = {
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+  console.log(data);
+  form.reset();
+});
